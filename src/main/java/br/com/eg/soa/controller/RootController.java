6d07b592cffa0026cc6bbdbcd769f61589e7808b
asponton <asponton@gmail.com>
2013-07-18 10:03:56 -0300

    heroku

--- src/main/java/br/com/eg/soa/controller/RootController.java
+++ src/main/java/br/com/eg/soa/controller/RootController.java
@@ -1,7 +1,5 @@
 package br.com.eg.soa.controller;
 
-import java.util.Calendar;
-
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
@@ -18,9 +16,7 @@ public class RootController {
 	
 	@Path("")
 	public void redirect() {
-		Calendar calendar = Calendar.getInstance();
-		result.include("data", calendar);
-		result.redirectTo("index.jsp");
+		result.redirectTo(HomeController.class).index();
 	}
 
 }
